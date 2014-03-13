6febfae6670f1c0ead276cc1a757bb9951db2b6a
textfield <textfield@0a995953-517f-4c62-b348-104708d926b2>
2014-03-13 10:33:01 +0000

    Dates in comments.
    git-svn-id: https://svn.code.sf.net/p/spot-on/code/branches/0.09.03@2802 0a995953-517f-4c62-b348-104708d926b2

--- Kernel/spot-on-starbeam-writer.h
+++ Kernel/spot-on-starbeam-writer.h
@@ -1,5 +1,5 @@
 /*
-** Copyright (c) 2011 10^10^10 Alexis Megas
+** Copyright (c) 2011 - 10^10^10 Alexis Megas
 ** All rights reserved.
 **
 ** Redistribution and use in source and binary forms, with or without
