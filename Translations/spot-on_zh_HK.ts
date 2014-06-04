afc1b2ec899aaf355b9b96418e10111b6cb75990
textfield <textfield@0a995953-517f-4c62-b348-104708d926b2>
2014-06-04 00:54:10 +0000

    Updated.
    git-svn-id: https://svn.code.sf.net/p/spot-on/code/branches/0.09.06@3026 0a995953-517f-4c62-b348-104708d926b2

--- Translations/spot-on_zh_HK.ts
+++ Translations/spot-on_zh_HK.ts
@@ -259,7 +259,7 @@
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../Common/spot-on-misc.cc" line="1068"/>
+        <location filename="../Common/spot-on-misc.cc" line="1105"/>
         <source>Sent</source>
         <translation type="unfinished"></translation>
     </message>
