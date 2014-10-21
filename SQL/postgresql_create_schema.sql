9bd9a52b5b1757c56fd2d685c50c60daf720f1f5
textfield <textfield@0a995953-517f-4c62-b348-104708d926b2>
2014-10-21 22:46:46 +0000

    New files.
    git-svn-id: https://svn.code.sf.net/p/spot-on/code/branches/0.16@3594 0a995953-517f-4c62-b348-104708d926b2

---
+++ SQL/postgresql_create_schema.sql
@@ -0,0 +1,19 @@
+-- CREATE USER spot_on_user PASSWORD 'spot_on_user';
+
+DO $$
+BEGIN
+	FOR i IN 1..26 LOOP
+	    FOR j IN 1..26 LOOP
+	    	EXECUTE('CREATE TABLE IF NOT EXISTS spot_on_urls_' ||
+			CHR(i + 96) || CHR(j + 96) ||
+	    		'(
+			date_time_inserted TEXT NOT NULL,
+			description BYTEA,
+			title BYTEA NOT NULL,
+			url BYTEA NOT NULL,
+			url_hash TEXT PRIMARY KEY NOT NULL
+	    		)');
+	    END LOOP;
+	END LOOP;
+END;
+$$;
