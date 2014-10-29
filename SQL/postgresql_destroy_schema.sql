44c49589c20901eb049b66f095a623f65cd2630a
textfield <textfield@0a995953-517f-4c62-b348-104708d926b2>
2014-10-29 19:05:22 +0000

    Updated.
    git-svn-id: https://svn.code.sf.net/p/spot-on/code/branches/0.16@3632 0a995953-517f-4c62-b348-104708d926b2

--- SQL/postgresql_destroy_schema.sql
+++ SQL/postgresql_destroy_schema.sql
@@ -1,18 +1,2 @@
 DROP DATABASE IF EXISTS spot_on_user_db;
-
-DO $$
-BEGIN
-	FOR i IN 1..26 LOOP
-	    FOR j IN 1..26 LOOP
-		EXECUTE('DROP TABLE IF EXISTS spot_on_keywords_' ||
-                        CHR(i + 96) || CHR(j + 96)
-                       );
-	    	EXECUTE('DROP TABLE IF EXISTS spot_on_urls_' ||
-			CHR(i + 96) || CHR(j + 96)
-	    	       );
-	    END LOOP;
-	END LOOP;
-END;
-$$;
-
 DROP USER IF EXISTS spot_on_user;
