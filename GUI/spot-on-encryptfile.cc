5fdcdc36ae94f09d319c1f87f0c3fbc88782edcb
textfield <textfield@0a995953-517f-4c62-b348-104708d926b2>
2014-12-30 11:06:57 +0000

    Removed CTR.
    git-svn-id: https://svn.code.sf.net/p/spot-on/code/branches/trunk@3944 0a995953-517f-4c62-b348-104708d926b2

--- GUI/spot-on-encryptfile.cc
+++ GUI/spot-on-encryptfile.cc
@@ -275,8 +275,6 @@ void spoton_encryptfile::slotConvert(void)
 
   if(ui.cbc->isChecked())
     modeOfOperation = "cbc";
-  else if(ui.ctr->isChecked())
-    modeOfOperation = "ctr";
   else
     modeOfOperation = "gcm";
 
